514417b0f1694f7afdf49a5ac17526f95b03e9f6
Jared Allard <jaredallard@outlook.com>
2018-04-03 16:47:56 -0700

    chore: remove scrips, move to development repo

--- secret.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "creating secret 'media-config' from ../config/config.yaml"
-kubectl delete secret media-config
-kubectl create secret generic media-config --from-file="config.yaml=../config/config.yaml"
